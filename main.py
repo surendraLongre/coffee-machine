6965cfa834144cd036d3e6d058dacb27b979f69c
surendraLongre <murari.longre2366@gmail.com>
2023-06-22 16:54:00 +0530

    add newer version change code from modular to oop based

--- main.py
+++ main.py
@@ -4,7 +4,19 @@ from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
 
 #start the main code
-while True:
-    user_input=input("What would you like? (espresso/latte/cappuccino): ")
-    if user_input=='report':
-    CoffeeMaker.report()
+make=CoffeeMaker()
+menu=Menu()
+is_on=True
+money_machine=MoneyMachine()
+while is_on:
+    user_input=input(f"What would you like? ({menu.get_items()}): ")
+    if user_input=='off':
+        is_on=False
+    elif user_input=='report':
+        make.report()
+    else:
+        drink=menu.find_drink(user_input)
+        
+        if make.is_resource_sufficient(drink) and money_machine.make_payment(drink.cost):
+            make.make_coffee(drink)
+
